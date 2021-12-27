1b9618a1ea576f8644c1585e300dc8d0af376491
Serhii Haluzov <sehal@smart-it.com>
2021-12-28 00:02:57 +0200

    init commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Parallel_Labs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
